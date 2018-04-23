46377bf5ec699c12f0251e75377568912aa01a79
jose <jose@autonomic.ai>
2018-04-23 13:46:11 -0400

    Implement a Cmd module

---
+++ Hline/Cmd.hs
@@ -0,0 +1,29 @@
+module Cmd
+( Cmd
+, resolveCmd
+, sh
+) where
+
+import System.Process
+import System.IO
+import qualified Data.Text as T
+
+data CmdHandle = CmdHandle { stdin :: Maybe Handle
+                           , stdout :: Maybe Handle
+                           , stderr :: Maybe Handle
+                           }
+
+data Cmd = Cmd { path :: T.Text } deriving (Show)
+
+sh :: Cmd -> [String] -> IO T.Text
+sh (Cmd path) args = (startProc (T.unpack path) args) >>= readHandle
+    where startProc cmd args = createProcess (proc cmd args) { std_out = CreatePipe }
+
+resolveCmd :: String -> IO (Maybe Cmd)
+resolveCmd cmd = which cmd >>= readHandle >>= isEmpty
+    where which cmd = createProcess (shell ("which " ++ cmd)){ std_out = CreatePipe }
+          isEmpty "" = return Nothing
+          isEmpty s = return (Just (Cmd s))
+
+readHandle :: (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle) -> IO T.Text
+readHandle (_, Just hout, _, phandle) = (waitForProcess phandle) >>= \_ -> (hGetContents hout >>= \s -> return (T.strip (T.pack s)))
