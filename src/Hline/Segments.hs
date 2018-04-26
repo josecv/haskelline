9e4a3359f1d94cce1171d80623d68b7bc3717e50
jose <jose@autonomic.ai>
2018-04-25 21:57:40 -0400

    functioning kubectl segment

--- src/Hline/Segments.hs
+++ src/Hline/Segments.hs
@@ -7,7 +7,8 @@ where
 import Hline.Cmd
 import Hline.SegmentExecutor
 import Hline.Prompt
--- import Control.Parallel
+import Control.Parallel
+import qualified Data.Text as T
 
 data KubeControl = KubeControl
 
@@ -15,4 +16,6 @@ instance SegmentExecutor KubeControl where
     execute KubeControl = (resolveCmd "kubectl") >>= buildSegment
         where buildSegment Nothing = return Nothing
               buildSegment (Just cmd) = getText cmd >>= return . Just . (\x -> Segment x (Just "white") (Just "magenta"))
-              getText cmd = sh cmd ["version"]
+              getText cmd = let namespace = (\c -> T.words ([t | t <- (T.lines c), (T.head t) == '*'] !! 0) !! 4) <$> (sh cmd ["config", "get-contexts", "--no-headers"])
+                                context = sh cmd ["config", "current-context"]
+                            in  par namespace $ pseq context $ (\x y -> if x /= y then T.concat [x, "/", y] else x) <$> context <*> namespace
