7241bd3c522be4f1a208c9c67aac1581505b299e
jose <jose@autonomic.ai>
2018-04-30 13:05:29 -0400

    first test case yay

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -1,7 +1,18 @@
 module Main where
 
+import Test.HUnit
+
 import Hline.Cmd
+import Data.Maybe
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+tests :: Test
+tests = TestList [TestLabel "resolveExisting" resolveCmdResolvesExisting, TestLabel "resolveNonExisting" resolveCmdFailsToResolveNonExisting]
 
+resolveCmdResolvesExisting :: Test
+resolveCmdResolvesExisting = TestCase $ isJust <$> resolveCmd "ghc" >>= assertBool "For resolveCmd ghc"
+
+resolveCmdFailsToResolveNonExisting :: Test
+resolveCmdFailsToResolveNonExisting = TestCase $ isNothing <$> resolveCmd "ghccccc" >>= assertBool "for resolveCmd with fake cmd"
+
+main :: IO Counts
+main = runTestTT tests
