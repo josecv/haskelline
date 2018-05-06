d58ca0308360bf0851c9470e29bb5111a869cc7a
jose <jose@autonomic.ai>
2018-05-06 18:09:36 -0400

    minor refactoring of buildLeftPrompt

--- src/Hline/Prompt.hs
+++ src/Hline/Prompt.hs
@@ -7,41 +7,40 @@ import qualified Data.Text as T
 import Data.Maybe
 import Hline.Icons
 
-data Segment = Segment { text' :: T.Text
-                       , fg' :: Maybe String
-                       , bg' :: Maybe String
+data Segment = Segment { text :: T.Text
+                       , fg :: Maybe String
+                       , bg :: Maybe String
                        } deriving Show
 
 data LeftAcc = LeftAcc T.Text (Maybe String)
              | First
 
 buildLeftPrompt :: [Maybe Segment] -> T.Text
-buildLeftPrompt segments =  renderAcc $ doFold $ catMaybes segments
-    where doFold = foldl folder First
-          renderAcc First = "" -- lol empty prompt
+buildLeftPrompt =  renderAcc . foldl folder First . catMaybes
+    where renderAcc First = "" -- lol empty prompt
           renderAcc (LeftAcc textAcc Nothing) = T.concat [promptClear, textAcc, bgEnd, fgEnd, " "]
           renderAcc (LeftAcc textAcc lastBg) = T.concat [promptClear, textAcc, bgEnd, fgFormat lastBg, icon "LEFT_SEGMENT_SEPARATOR", fgEnd, " "]
           folder First segment = LeftAcc (firstSegment segment) (getBg segment)
-          folder (LeftAcc textAcc lastBg) (Segment text fg bg) = LeftAcc (T.append textAcc (renderText lastBg (Segment text fg bg))) bg
+          folder (LeftAcc textAcc lastBg) (Segment text' fg' bg') = LeftAcc (T.append textAcc (renderText lastBg (Segment text' fg' bg'))) bg'
           renderText Nothing segment = firstSegment segment
-          renderText lastBg (Segment text fg bg)
-                | lastBg == bg = T.concat [bgFormat bg, fgFormat fg, icon "LEFT_SUBSEGMENT_SEPARATOR", " ", showContent text fg, " "]
-                | otherwise = T.concat [bgFormat bg, fgFormat lastBg, icon "LEFT_SEGMENT_SEPARATOR", " ", showContent text fg, " "]
-          firstSegment (Segment text fg bg) = T.concat [bgFormat bg,  " ", showContent text fg, " "]
+          renderText lastBg (Segment text' fg' bg')
+                | lastBg == bg' = T.concat [bgFormat bg', fgFormat fg', icon "LEFT_SUBSEGMENT_SEPARATOR", " ", showContent text' fg', " "]
+                | otherwise = T.concat [bgFormat bg', fgFormat lastBg, icon "LEFT_SEGMENT_SEPARATOR", " ", showContent text' fg', " "]
+          firstSegment (Segment text' fg' bg') = T.concat [bgFormat bg',  " ", showContent text' fg', " "]
 
 getBg :: Segment -> Maybe String
-getBg (Segment _ _ bg) = bg
+getBg (Segment _ _ bg') = bg'
 
 showContent :: T.Text -> Maybe String -> T.Text
-showContent text fg = (fgFormat fg) `T.append` text
+showContent text' fg' = (fgFormat fg') `T.append` text'
 
 bgFormat :: Maybe String -> T.Text
 bgFormat Nothing = "%k"
-bgFormat (Just bg) = T.concat ["%K{", (T.pack bg), "}"]
+bgFormat (Just bg') = T.concat ["%K{", (T.pack bg'), "}"]
 
 fgFormat :: Maybe String -> T.Text
 fgFormat Nothing = "%f"
-fgFormat (Just fg) = T.concat ["%F{", (T.pack fg), "}"]
+fgFormat (Just fg') = T.concat ["%F{", (T.pack fg'), "}"]
 
 bgEnd :: T.Text
 bgEnd = "%k"
